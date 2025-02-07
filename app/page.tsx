6b1f7608d188572bca3da8780537541b52002d74
Athira Divakar <athira.di@pitsolutions.com>
2025-02-07 10:03:54 +0530

    optimizing fonts and images

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from './ui/fonts';
+import Image from 'next/image';
 
 export default function Page() {
   return (
@@ -18,9 +19,7 @@ export default function Page() {
           />
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className} antialiased`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
+            <Link href="https://nextjs.org/learn/" className="text-blue-500">Next.js Learn Course</Link>
             , brought to you by Vercel.
           </p>
           <Link
@@ -32,6 +31,8 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image src="/hero-desktop.png" width={1000} height={760} className='hidden md:block' alt='Screenshots of the dashboard project showing desktop version' />
+          <Image src="/hero-mobile.png" width={560} height={620} className='block md:hidden' alt='Screenshots of the dashboard project showing mobile version' />
         </div>
       </div>
     </main>
