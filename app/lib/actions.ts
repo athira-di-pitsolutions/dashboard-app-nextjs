0a4bc68e467b72a09356bb7a5279accae97edd07
Athira Divakar <athira.di@pitsolutions.com>
2025-02-11 15:22:38 +0530

    authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import postgres from 'postgres';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
@@ -103,4 +105,22 @@ export async function deleteInvoice(id: string) {
     }
 
     revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            if (error.type === 'CredentialsSignin') {
+                    return 'Invalid credentials.';
+            } else {
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
+    }
 }
